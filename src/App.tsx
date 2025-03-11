b8090cebb325ac97da0c1af0ab7c06f38c7b6e14
Maddox Florez <maddox@udel.edu>
2025-03-10 22:14:51 -0400

    unrelated histories

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -11,8 +10,6 @@ import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
-=======
->>>>>>> origin/main
 
 function App(): React.JSX.Element {
     return (
@@ -20,7 +17,6 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
             <hr></hr>
             <DoubleHalf></DoubleHalf>
             <hr></hr>
@@ -41,12 +37,6 @@ function App(): React.JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
-=======
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
->>>>>>> origin/main
         </div>
     );
 }
